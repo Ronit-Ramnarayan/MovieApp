57aff585233149840574287df4ca3682c833356f
Ronit Ramnarayan <ronitvjramnarayan@gmail.com>
2024-10-08 11:23:08 -0400

    Add project files.

---
+++ MovieApp/Models/Actor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        public Actor(int id, string profilePic, string fullName, string bio)
+        {
+            Id = id;
+            ProfilePictureURL = profilePic;
+            FullName = fullName;
+            Bio = bio;
+        }
+    }
+}
