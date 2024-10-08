1b7bf599c05757c9cafb054de0ac0390d22ac6cb
Ronit Ramnarayan <ronitvjramnarayan@gmail.com>
2024-10-08 11:34:20 -0400

    Created rest of models and DbContext

--- MovieApp/Models/Cinema.cs
+++ MovieApp/Models/Cinema.cs
@@ -1,10 +1,21 @@
-﻿namespace MovieApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Models
 {
     public class Cinema
     {
+        [Key]
         public int Id { get; set; }
         public string Logo { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public Cinema(int id, string logo, string name, string description)
+        {
+            Id = id;
+            Logo = logo;
+            Name = name;
+            Description = description;
+        }
     }
 }
---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MovieApp.Data.Enum;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+
+        public Movie(int id, string name, string description, double price, string imageURL, DateTime startDate, DateTime endDate, MovieCategory movieCategory)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
+            ImageURL = imageURL;
+            StartDate = startDate;
+            EndDate = endDate;
+            MovieCategory = movieCategory;
+        }
+    }
+}
