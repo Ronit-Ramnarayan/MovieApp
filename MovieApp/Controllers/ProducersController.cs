770e1559447c0f9e7c07940b4376613210d41449
Ronit Ramnarayan <ronitvjramnarayan@gmail.com>
2024-10-09 00:45:01 -0400

    Created Actor and Producer controllers

---
+++ MovieApp/Controllers/ProducersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Data;
+
+namespace MovieApp.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Producers.ToList();
+            return View();
+        }
+    }
+}
